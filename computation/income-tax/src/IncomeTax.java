46078db35c50fbed5430ff462b714f5bb8b03cd8
Christel Hamers <christelhamers@gmail.com>
2022-06-25 14:31:09 +0100

    computation folder added

---
+++ computation/income-tax/src/IncomeTax.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class IncomeTax {
+    static void nonSavingsIncome() throws IOException{
+        System.out.println("Non-savings income comprises employment income, ......");
+        System.out.println("Put in your employment income:");
+        System.in.read();
+    }
+}
